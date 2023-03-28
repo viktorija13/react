a2072276a9bfebce22d333ee19cfcb83f8d0c87d
viktorija13 <viktorijaabrocic@gmail.com>
2023-03-28 19:34:36 +0200

    Ostatak

---
+++ src/Loading.js
@@ -0,0 +1,12 @@
+// react imports
+import React from 'react';
+
+const Loading = () => {
+  return (
+    <main>
+      <div className='loading'></div>
+    </main>
+  );
+};
+
+export default Loading;
